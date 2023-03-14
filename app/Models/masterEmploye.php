df6f7619dbb2fceef7c14a5d4eaa3c769b4806a5
yosef <yosefdarmawan1@gmail.com>
2023-03-14 15:57:16 +0700

    fix

--- app/Models/masterEmploye.php
+++ app/Models/masterEmploye.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class masterEmploye extends Model
+class masterEmploye extends Authenticatable
 {
     use HasFactory;
     protected $table="master_employe";
