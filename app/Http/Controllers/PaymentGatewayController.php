cdd28dadbfc3f1d36ecbe12d81ae490fe65d7da3
YuliAsa33 <yuliasaalfalah33@gmail.com>
2023-06-12 13:39:24 +0700

    Callback UI

--- app/Http/Controllers/PaymentGatewayController.php
+++ app/Http/Controllers/PaymentGatewayController.php
@@ -13,221 +13,226 @@ use Illuminate\Support\Str;
 class PaymentGatewayController extends Controller
 {
     //
-    
-    public function registrasi(Request $request){
-        
-        try{
-        
-            $order = transOrder::where('uuid',$request->uuid)
-            ->with('transOrderDetail.master_menu','user')->first();
+
+    public function registrasi(Request $request)
+    {
+
+        try {
+
+            $order = transOrder::where('uuid', $request->uuid)
+                ->with('transOrderDetail.master_menu', 'user')->first();
             // dd($order);
             $timeStamp = date('YmdHIs');
             $iMid = "IONPAYTEST";
             $merchantKey = "33F49GnCMS1mFYlGXisbUDzVf2ATWCl9k3R++d5hDd3Frmuos/XLx8XhXpe+LDYAbpGKZYSwtlyyLOtS/8aD7A==";
             $reffno = $order->nomor_order;
             $amount = $order->total_price;
-            $merchantData = $timeStamp.$iMid.$reffno.$amount.$merchantKey;
-            $merTok = hash('sha256',$merchantData);
+            $merchantData = $timeStamp . $iMid . $reffno . $amount . $merchantKey;
+            $merTok = hash('sha256', $merchantData);
             // dd($order->toArray());
             $item = [];
-            foreach($order->transOrderDetail as $detail){
+            foreach ($order->transOrderDetail as $detail) {
                 $item[] = [
-                    "goods_id" => $detail->id_menu, 
-                    "goods_detail" => $detail->master_menu->name, 
-                    "goods_name" => $detail->master_menu->name, 
-                    "goods_amt" => $detail->total_price, 
-                    "goods_type" => $detail->master_menu->name, 
-                    "goods_url" => $detail->master_menu->url_image, 
+                    "goods_id" => $detail->id_menu,
+                    "goods_detail" => $detail->master_menu->name,
+                    "goods_name" => $detail->master_menu->name,
+                    "goods_amt" => $detail->total_price,
+                    "goods_type" => $detail->master_menu->name,
+                    "goods_url" => $detail->master_menu->url_image,
                     "goods_quantity" => 1
                 ];
             }
-            
-            $cart =[
-                "count" => count($order->transOrderDetail), 
+
+            $cart = [
+                "count" => count($order->transOrderDetail),
                 "item" => $item
             ];
             $date = strtotime("+1 day");
             $body = [
-                "timeStamp" => $timeStamp, 
-                "iMid" => $iMid, 
-                "payMethod" => "00", 
-                "bankCd" => "CENA", 
-                "currency" => "IDR", 
-                "amt" => $amount, 
-                "referenceNo" => $reffno, 
-                "merchantToken" => $merTok, 
-                "callBackUrl" => "https://dpatriotcafe.com/api/payment/callBackUrl", 
-                "dbProcessUrl" => "https://dpatriotcafe.com/api/payment/notification", 
-                "goodsNm" => "makanan/minuman", 
-                "mitraCd" => "", 
-                "vacctValidDt" => date('Ymd',$date), 
-                "vacctValidTm" => date('His'), 
-                "description" => "Pesanan Makanan Dan Minuman", 
-                "billingNm" => $order->user->name, 
-                "billingPhone" => $order->user->phone, 
-                "billingEmail" => $order->user->email, 
-                "billingAddr" => "", 
-                "billingCity" => "semarang", 
-                "billingState" => "jawa tengah", 
-                "billingPostCd" => "50198", 
-                "billingCountry" => "indonesia", 
-                "userIP" => "127.0.0.1", 
-                "cartData" => json_encode($cart), 
+                "timeStamp" => $timeStamp,
+                "iMid" => $iMid,
+                "payMethod" => "00",
+                "bankCd" => "CENA",
+                "currency" => "IDR",
+                "amt" => $amount,
+                "referenceNo" => $reffno,
+                "merchantToken" => $merTok,
+                "callBackUrl" => "https://dpatriotcafe.com/api/payment/callBackUrl",
+                "dbProcessUrl" => "https://dpatriotcafe.com/api/payment/notification",
+                "goodsNm" => "makanan/minuman",
+                "mitraCd" => "",
+                "vacctValidDt" => date('Ymd', $date),
+                "vacctValidTm" => date('His'),
+                "description" => "Pesanan Makanan Dan Minuman",
+                "billingNm" => $order->user->name,
+                "billingPhone" => $order->user->phone,
+                "billingEmail" => $order->user->email,
+                "billingAddr" => "",
+                "billingCity" => "semarang",
+                "billingState" => "jawa tengah",
+                "billingPostCd" => "50198",
+                "billingCountry" => "indonesia",
+                "userIP" => "127.0.0.1",
+                "cartData" => json_encode($cart),
                 "deliveryNm" => "",
-                "deliveryPhone" => "", 
-                "deliveryAddr" => "", 
-                "deliveryCity" => "", 
-                "deliveryState" => "", 
-                "deliveryPostCd" => "", 
-                "deliveryCountry" => "", 
-                "vat" => "", 
-                "fee" => "", 
-                "notaxAmt" => "", 
-                "reqDt" => "", 
-                "reqTm" => "", 
-                "reqDomain" => "", 
-                "reqServerIP" => "", 
-                "reqClientVer" => "", 
-                "userSessionID" => "", 
-                "userAgent" => "", 
-                "userLanguage" => "" 
-            ];
-            
-            $response = Http::withHeaders([
-                'Content-Type' => 'application/json',
-            ])->post('https://dev.nicepay.co.id/nicepay/redirect/v2/registration',$body);
-            
-            $result = $response->object();
-            
-            if($result->resultCd!='0000'){
-                throw new \Exception('error payment gateway '.$result->resultMsg);
-            }
-                        
-            $notif = new TransInvoice;
-            $notif->uuid = Str::uuid();
-            $notif->referenceNo =$result->referenceNo;
-            $notif->tXid =$result->tXid;
-            $notif->payMethod = '';
-            $notif->body =json_encode($result);
-            $notif->save();
-            
-            $data = [
-                "result" =>$result,
-                "paymentURL"=>$result->paymentURL.'?tXid='.$result->tXid,
+                "deliveryPhone" => "",
+                "deliveryAddr" => "",
+                "deliveryCity" => "",
+                "deliveryState" => "",
+                "deliveryPostCd" => "",
+                "deliveryCountry" => "",
+                "vat" => "",
+                "fee" => "",
+                "notaxAmt" => "",
+                "reqDt" => "",
+                "reqTm" => "",
+                "reqDomain" => "",
+                "reqServerIP" => "",
+                "reqClientVer" => "",
+                "userSessionID" => "",
+                "userAgent" => "",
+                "userLanguage" => ""
             ];
 
-            return response()->json(['status'=>true,'data'=>$data]);
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post('https://dev.nicepay.co.id/nicepay/redirect/v2/registration', $body);
+
+            $result = $response->object();
+
+            if ($result->resultCd != '0000') {
+                throw new \Exception('error payment gateway ' . $result->resultMsg);
+            }
+
+            $notif = new TransInvoice;
+            $notif->uuid = Str::uuid();
+            $notif->referenceNo = $result->referenceNo;
+            $notif->tXid = $result->tXid;
+            $notif->payMethod = '';
+            $notif->body = json_encode($result);
+            $notif->save();
+
+            $data = [
+                "result" => $result,
+                "paymentURL" => $result->paymentURL . '?tXid=' . $result->tXid,
+            ];
+
+            return response()->json(['status' => true, 'data' => $data]);
         } catch (\Exception $ex) {
-            return response()->json(['status'=>false,'data'=>[],'message'=>$ex->getMessage()]);
+            return response()->json(['status' => false, 'data' => [], 'message' => $ex->getMessage()]);
         }
     }
-    
-    public function qris(Request $request){
-        
-        try{
-        
-            $order = transOrder::where('uuid',$request->uuid)
-            ->with('transOrderDetail.master_menu','master_customer')->first();
-            
+
+    public function qris(Request $request)
+    {
+
+        try {
+
+            $order = transOrder::where('uuid', $request->uuid)
+                ->with('transOrderDetail.master_menu', 'master_customer')->first();
+
             $timeStamp = date('YmdHIs');
             $iMid = "IONPAYTEST";
             $merchantKey = "33F49GnCMS1mFYlGXisbUDzVf2ATWCl9k3R++d5hDd3Frmuos/XLx8XhXpe+LDYAbpGKZYSwtlyyLOtS/8aD7A==";
             $reffno = $order->nomor_order;
             $amount = $order->total_price;
-            $merchantData = $timeStamp.$iMid.$reffno.$amount.$merchantKey;
-            $merTok = hash('sha256',$merchantData);
+            $merchantData = $timeStamp . $iMid . $reffno . $amount . $merchantKey;
+            $merTok = hash('sha256', $merchantData);
             // dd($order->toArray());
             $item = [];
-            foreach($order->transOrderDetail as $detail){
+            foreach ($order->transOrderDetail as $detail) {
                 $item[] = [
-                    "goods_id" => $detail->id_menu, 
-                    "goods_detail" => $detail->master_menu->name, 
-                    "goods_name" => $detail->master_menu->name, 
-                    "goods_amt" => $detail->total_price, 
-                    "goods_type" => $detail->master_menu->name, 
-                    "goods_url" => $detail->master_menu->url_image, 
+                    "goods_id" => $detail->id_menu,
+                    "goods_detail" => $detail->master_menu->name,
+                    "goods_name" => $detail->master_menu->name,
+                    "goods_amt" => $detail->total_price,
+                    "goods_type" => $detail->master_menu->name,
+                    "goods_url" => $detail->master_menu->url_image,
                     "goods_quantity" => 1
                 ];
             }
-            
-            $cart =[
-                "count" => count($order->transOrderDetail), 
+
+            $cart = [
+                "count" => count($order->transOrderDetail),
                 "item" => $item
             ];
-            
+
             $body = [
-                "timeStamp" => $timeStamp, 
-                "iMid" => $iMid, 
-                "payMethod" => "08", 
-                "currency" => "IDR", 
-                "amt" => $amount, 
+                "timeStamp" => $timeStamp,
+                "iMid" => $iMid,
+                "payMethod" => "08",
+                "currency" => "IDR",
+                "amt" => $amount,
                 "referenceNo" => $reffno,
-                "goodsNm" => "Pesanan Makanan Dan Minuman", 
-                "billingNm" => $order->user->name, 
-                "billingPhone" => $order->user->phone, 
-                "billingEmail" => $order->user->email, 
-                "billingAddr" => "", 
-                "billingCity" => "semarang", 
-                "billingState" => "jawa tengah", 
-                "billingPostCd" => "50198", 
-                "billingCountry" => "indonesia",  
+                "goodsNm" => "Pesanan Makanan Dan Minuman",
+                "billingNm" => $order->user->name,
+                "billingPhone" => $order->user->phone,
+                "billingEmail" => $order->user->email,
+                "billingAddr" => "",
+                "billingCity" => "semarang",
+                "billingState" => "jawa tengah",
+                "billingPostCd" => "50198",
+                "billingCountry" => "indonesia",
                 "dbProcessUrl" => "https://dpatriotcafe.com/api/payment/notification",
-                "merchantToken" => $merTok,  
-                "paymentExpDt" => "", 
-                "paymentExpTm" => "", 
-                "userIP" => "127.0.0.1", 
-                "cartData" => json_encode($cart), 
-                "mitraCd" => "QSHP", 
-                "shopId" => "NICEPAY" 
-            ]; 
-            
-            $response = Http::withHeaders([
-                'Content-Type' => 'application/json',
-            ])->post('https://dev.nicepay.co.id/nicepay/direct/v2/registration',$body);
-            
-            $result = $response->object();
-            
-            if($result->resultCd!='0000'){
-                throw new \Exception('error payment gateway '.$result->resultMsg);
-            }
-                        
-            $notif = new TransInvoice;
-            $notif->uuid = Str::uuid();
-            $notif->referenceNo =$result->referenceNo;
-            $notif->tXid =$result->tXid;
-            $notif->payMethod = '';
-            $notif->body =json_encode($result);
-            $notif->save();
-            
-            $data = [
-                "result" =>$result,
-                "QRIS"=>$result->qrContent,
+                "merchantToken" => $merTok,
+                "paymentExpDt" => "",
+                "paymentExpTm" => "",
+                "userIP" => "127.0.0.1",
+                "cartData" => json_encode($cart),
+                "mitraCd" => "QSHP",
+                "shopId" => "NICEPAY"
             ];
 
-            return response()->json(['status'=>true,'data'=>$data]);
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post('https://dev.nicepay.co.id/nicepay/direct/v2/registration', $body);
+
+            $result = $response->object();
+
+            if ($result->resultCd != '0000') {
+                throw new \Exception('error payment gateway ' . $result->resultMsg);
+            }
+
+            $notif = new TransInvoice;
+            $notif->uuid = Str::uuid();
+            $notif->referenceNo = $result->referenceNo;
+            $notif->tXid = $result->tXid;
+            $notif->payMethod = '';
+            $notif->body = json_encode($result);
+            $notif->save();
+
+            $data = [
+                "result" => $result,
+                "QRIS" => $result->qrContent,
+            ];
+
+            return response()->json(['status' => true, 'data' => $data]);
         } catch (\Exception $ex) {
-            return response()->json(['status'=>false,'data'=>[],'message'=>$ex->getMessage()]);
+            return response()->json(['status' => false, 'data' => [], 'message' => $ex->getMessage()]);
         }
     }
-    
-    public function notification(Request $request){
-        try{
+
+    public function notification(Request $request)
+    {
+        try {
             $notif = new TransInvoiceNotif();
             $notif->uuid = Str::uuid();
-            $notif->referenceNo =$request->referenceNo;
-            $notif->tXid =$request->tXid;
+            $notif->referenceNo = $request->referenceNo;
+            $notif->tXid = $request->tXid;
             $notif->payMethod = $request->payMethod;
-            $notif->body =json_encode($request->all());
+            $notif->body = json_encode($request->all());
             $notif->save();
-            
-            return response()->json(['status'=>true,'data'=>$notif]);
+
+            return response()->json(['status' => true, 'data' => $notif]);
         } catch (\Exception $ex) {
-            return response()->json(['status'=>false,'data'=>[],'message'=>$ex->getMessage()]);
+            return response()->json(['status' => false, 'data' => [], 'message' => $ex->getMessage()]);
         }
     }
-    
-    public function callBackUrl(Request $request){
+
+    public function callBackUrl(Request $request)
+    {
         $data = $request->all();
-        return view('paymentGateway.callbackUrl',['data'=>$data]);
+
+        return view('paymentGateway/callbackUrl', ['data' => $data]);
     }
 }
